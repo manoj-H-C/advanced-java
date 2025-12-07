4a7058b4be99cda39e2fd160218d117f05347848
Manoj H C <manojhc110@gmail.com>
2025-12-08 01:30:51 +0530

    stream terminal operations done

---
+++ src/a4streams/terminal_operations/StreamsJoiningExample.java
@@ -0,0 +1,44 @@
+package a4streams.terminal_operations;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
+public class StreamsJoiningExample {
+    public static String joining(){
+
+        return StudentDataBase.getAllStudents().stream()
+                .map(Student::getName)
+                .collect(Collectors.joining()); // appends all the strings to produce the output,
+    }
+
+    public static String joiningGender(){
+        return StudentDataBase.getAllStudents().stream()
+                .map(Student::getGender)
+                .distinct()
+                .collect(Collectors.joining("_","{","}"));
+    }
+
+    public static String joiningWithDelimiter(){
+
+        return StudentDataBase.getAllStudents().stream()
+                .map(Student::getName)
+                .collect(Collectors.joining("-"));
+    }
+
+    public static String joiningWithDelimiterWithPrefix(){
+
+        return StudentDataBase.getAllStudents().stream()
+                .map(Student::getName)
+                .collect(Collectors.joining("-","[","]"));
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Names : "+ joining());
+        System.out.println("Names With Delimiter : "+ joiningWithDelimiter());
+        System.out.println("Names With Delimiter Prefix and Suffix : "+ joiningWithDelimiterWithPrefix());
+        System.out.println("gender : "+ joiningGender());
+    }
+}
---
+++ src/a4streams/terminal_operations/StreamsMappingExample.java
@@ -0,0 +1,35 @@
+package a4streams.terminal_operations;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.*;
+
+public class StreamsMappingExample {
+    public static void main(String[] args) {
+
+        Set<String> namesSet = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(mapping(Student::getName,toSet())); // this avoids the additional map intermediate operation.
+
+        System.out.println("namesSet : " + namesSet);
+
+        List<String> namesList = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(mapping(Student::getName,toList())); // this avoids the additional map intermediate operation.
+
+        System.out.println("namesList : " + namesList);
+
+        System.out.println("the above are same as : ");
+
+        List<String> namesListMap = StudentDataBase.getAllStudents().stream()
+                .map(Student::getName)
+                .collect(toList());
+
+        System.out.println(namesListMap);
+
+    }
+}
